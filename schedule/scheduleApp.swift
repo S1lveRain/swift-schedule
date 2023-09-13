c432316ae332d05293bccffa3aeb6f63320fb72f
Rain <silverain1qw@gmail.com>
2023-09-13 12:35:18 +0300

    Initial Commit

---
+++ schedule/scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  scheduleApp.swift
+//  schedule
+//
+//  Created by Raindesu on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
